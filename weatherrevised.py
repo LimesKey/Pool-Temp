a720dbfbee0535457215a5e0e20f7de67c2a417d
iaacornus <iaacornus.devel@gmail.com>
2022-10-02 18:35:12 +0800

    fix lines that has more than 80 chars

--- weatherrevised.py
+++ weatherrevised.py
@@ -1,19 +1,22 @@
 import requests
-import math
 import time
-import datetime
 
 
 # intro
-print("This program is for finding the best pool temperature to set your heater at to save the most amount of money "
-      "whilst still having a good temperature pool.")
+print(
+    (
+        "This program is for finding the best pool temperature"
+        " to set your heater at to save the most amount of money"
+        " whilst still having a good temperature pool."
+    )
+)
 
 city_name = str(input("What City or Town do you live in?\n")).lower()
 country_name = str(input("What country do you live in?\n")).upper()
 api_key = input("What's your ApiKey for OpenWeatherMap?\n").lower()
 
 time2 = time.strftime("%H%M", time.localtime())  # get time
-if api_key == '':
+if api_key == "":
     api_key = ""
 
 swim_or_not = False
@@ -24,14 +27,27 @@ if city_name == "milton":
 
 
 def get_weather(city_name, country_name, api_key):
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}, {country_name}&appid={api_key}&units=metric"
+    url = (
+            "https://api.openweathermap.org/data/2.5/weather"
+            f"?q={city_name}, {country_name}&appid={api_key}&units=metric"
+        )
     response = requests.get(url).json()
-    while 'cod' in response and response['cod'] == '401':
-        api_key = input('Your API Key was incorrect, please enter it again. You may need to wait a few hours for it to activate.').lower()
-    if 'cod' in response and response['cod'] == '404':
-        print('You can get this error when you specified the wrong city name, ZIP-code or city ID. For your '
-              'reference, this list contains City name, City ID, Geographical coordinates of the city (lon, lat), '
-              'Zoom, etc. We are sorry.')
+    while "cod" in response and response["cod"] == "401":
+        api_key = input(
+                (
+                    "Your API Key was incorrect, please enter it again. "
+                    "You may need to wait a few hours for it to activate."
+                )
+            ).lower()
+    if "cod" in response and response["cod"] == "404":
+        print(
+            (
+                "You can get this error when you specified the wrong "
+                "city name, ZIP-code or city ID. For your reference, this"
+                " list contains City name, City ID, Geographical "
+                "coordinates of the city (lon, lat), Zoom, etc. We are sorry."
+            )
+        )
     else:
         global current_temp
         global feels_like
@@ -41,28 +57,28 @@ def get_weather(city_name, country_name, api_key):
         global units
         global sunset
         global sunset_unix
-        if response['sys']['country'] == 'US':
-            units = 'F'
+        if response["sys"]["country"] == "US":
+            units = "F"
         else:
-            units = 'C'
-        sunset_unix = response['sys']['sunset']
+            units = "C"
+        sunset_unix = response["sys"]["sunset"]
         sunset = time.strftime("%I:%M %p", time.localtime(int(sunset_unix)))  # %I is for 24 hour time, %p is for AM/PM time
-        sunset = sunset.lstrip('0')  # strips the 0 in front of the hour
-        current_temp = round(response['main']['temp'])  # get temp
-        print(f'The current temp is {current_temp}.')
+        sunset = sunset.lstrip("0")  # strips the 0 in front of the hour
+        current_temp = round(response["main"]["temp"])  # get temp
+        print(f"The current temp is {current_temp}.")
 
-        feels_like = response['main']["feels_like"]  # get feel like
+        feels_like = response["main"]["feels_like"]  # get feel like
         feels_like = round(feels_like)
         print(f"This is feels like {feels_like}.")
 
         humidity = round(response["main"]["humidity"])
-        print(f'The humidity is {humidity}%.')
+        print(f"The humidity is {humidity}%.")
 
         global partial_swim
         partial_swim = False
 
-        for item2 in response['weather']:
-            current_weather = item2['id']
+        for item2 in response["weather"]:
+            current_weather = item2["id"]
             if current_weather in [801, 802, 800]:  # good weather for swimming
                 partial_swim = False
             else:
@@ -70,7 +86,7 @@ def get_weather(city_name, country_name, api_key):
 
         global warning
         try:
-            warning = response['alerts']
+            warning = response["alerts"]
             swim_or_not = True
         except KeyError:
             pass
@@ -84,30 +100,43 @@ def get_weather(city_name, country_name, api_key):
 
 
 def get_forcast(city_name, country_name, api_key):
-    url2 = f"https://api.openweathermap.org/data/2.5/forecast?q={city_name},{country_name}&appid={api_key}&units=metric"
+    url2 = (
+            "https://api.openweathermap.org/data/2.5/forecast"
+            f"?q={city_name},{country_name}&appid={api_key}&units=metric"
+        )
     response2 = requests.get(url2).json()
-    if 'cod' in response2 and response2['cod'] != '404' or '404':
+    if "cod" in response2 and response2["cod"] != "404" or "404":
         global feels_like_3_hour
-        feels_like_3_hour = round(response2['list'][0]["main"]["feels_like"])
-        print(f'The air temp in 3 hours will be {feels_like_3_hour}c')
+        feels_like_3_hour = round(response2["list"][0]["main"]["feels_like"])
+        print(f"The air temp in 3 hours will be {feels_like_3_hour}c")
         global humidity_in_3_hour
-        humidity_in_3_hour = round(response2['list'][0]['main']['humidity'])
-        print(f'The humidity in 3 hours will be {humidity_in_3_hour}.')
+        humidity_in_3_hour = round(response2["list"][0]["main"]["humidity"])
+        print(f"The humidity in 3 hours will be {humidity_in_3_hour}.")
     else:
-        print('party')
+        print("party")
 
 try:
     get_forcast(city_name, country_name, api_key)
     get_weather(city_name, country_name, api_key)
 except TypeError:
-    print(f'Sorry something went wrong, error code: {TypeError}')
+    print(f"Sorry something went wrong, error code: {TypeError}")
 except KeyError:
-    print(f'Sorry something went wrong, error code {TypeError}.')
+    print(f"Sorry something went wrong, error code {TypeError}.")
 
 
 # for actually deciding what pool temp to set the heater at
-def calculate_temp(current_temp, humidity, feels_like_3_hour, feels_like, pool_temp_normal, humidity_in_3_hour, swim_or_not, partial_swim, sunset, sunset_unix):
-    if units == 'F':
+def calculate_temp(
+        current_temp,
+        humidity,
+        feels_like_3_hour,
+        feels_like,
+        pool_temp_normal,
+        humidity_in_3_hour,
+        swim_or_not, partial_swim,
+        sunset,
+        sunset_unix
+    ):
+    if units == "F":
         current_temp *= 1.8 + 32
         feels_like *= 1.8 + 32
         feels_like_3_hour *= 1.8 + 32
@@ -120,31 +149,61 @@ def calculate_temp(current_temp, humidity, feels_like_3_hour, feels_like, pool_t
     if feels_like_3_hour < current_temp and humidity > humidity_in_3_hour:
         pool_temp_normal += 1
         partial_swim = True
-    calcuate_sunrise_comparison2 = time.strftime("%H%M", time.localtime(sunset_unix))
+    calcuate_sunrise_comparison2 = time.strftime(
+            "%H%M", time.localtime(sunset_unix)
+        )
     atnighttrue = False
-    atnight = ''
+    atnight = ""
     if time2 > calcuate_sunrise_comparison2:
-        atnight = 'Its currently dark outside so I wouldn\'t recommend swimming'
+        atnight = (
+                "Its currently dark outside so I wouldn\"t recommend swimming"
+            )
         atnighttrue = True
     return atnighttrue, atnight
 
 
 try:
-    atnighttrue, atnight = calculate_temp(current_temp, humidity, feels_like_3_hour, feels_like, pool_temp_normal, humidity_in_3_hour, swim_or_not, partial_swim, sunset, sunset_unix)
+    atnighttrue, atnight = calculate_temp(
+            current_temp,
+            humidity,
+            feels_like_3_hour,
+            feels_like,
+            pool_temp_normal,
+            humidity_in_3_hour,
+            swim_or_not,
+            partial_swim,
+            sunset,
+            sunset_unix
+        )
 except TypeError:
-    print(f'Sorry something went wrong, error code: {TypeError}')
+    print(f"Sorry something went wrong, error code: {TypeError}")
 
-if units == 'F':
+if units == "F":
     pool_temp_normal *= 1.8 + 32
 
 if swim_or_not:
-    print(f"I cannot give you a temperature to set your pool heater at because of dangerous weather concerns in your area. The "
-          f"weather concern is {warning}.")
+    print(
+        (
+            "I cannot give you a temperature to set your pool"
+            " heater at because of dangerous weather concerns "
+            f"in your area. The weather concern is {warning}."
+        )
+    )
 elif partial_swim:
     if atnighttrue:
-        print(f'Your suggested pool heater temperature is {pool_temp_normal}{units} but {atnight}.')
+        print(
+            "Your suggested pool heater temperature "
+            f"is {pool_temp_normal}{units} but {atnight}."
+        )
     else:
-        print(f'Your suggested pool heater temperature is {pool_temp_normal}{units}, but I wouldn\'t recommend you to swim at this time.')
+        print(
+            "Your suggested pool heater temperature is "
+            f"{pool_temp_normal}{units}, but I wouldn't"
+            " recommend you to swim at this time."
+        )
 else:
-    print(f'Your suggested pool heater temperature is {pool_temp_normal}{units}, there where no weather concerns in '
-          f'your area.')
+    print(
+        "Your suggested pool heater temperature is "
+        f"{pool_temp_normal}{units}, there where no"
+        " weather concerns in your area."
+    )
